9941553a611b1b988d7e38479816514803d8ce48
Amar Hodzic <amar_hodzic@hotmail.com>
2020-10-20 15:30:53 +0200

    Repo init

---
+++ Assets/Audio/AudioManager.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour
+{
+    public SoundHelper[] sounds;
+    private string music = "musicEnabled";
+    public bool castleMusic = true;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+
+
+        //setting the audio managers parameters equals to my parameters 
+        foreach (SoundHelper s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+         
+        }
+        
+    }
+    void Start()
+    {
+
+
+
+        ActivateMusic();
+        if (gameObject.activeSelf)
+        {
+            StartCoroutine(playsound(castleMusic));
+        }
+       
+       
+       
+      
+
+
+    }
+
+    public void Play(string name)
+    {
+        SoundHelper s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.Log("Sound isnt there");
+            return;
+        }
+        s.source.Play();
+        s.isPlaying = true;
+
+    }
+    //function that returns the length of the sound
+    private float Soundlength(string name)
+    {
+        SoundHelper s = Array.Find(sounds, sound => sound.name == name);
+        float length = s.clip.length;
+        return length;
+    }
+
+    //used to play the two diffrent background sounds one after another without delay
+    IEnumerator playsound(bool music)
+    {
+        if (music) {
+            float waitTime = Soundlength("MusicStart") - 2f;
+            Play("MusicStart");
+            yield return new WaitForSeconds(waitTime);
+            Play("MusicLoop");
+        }
+        else
+        {
+            float waitTime = Soundlength("MusicStartForest") -4f;
+            Play("MusicStartForest");
+            yield return new WaitForSeconds(waitTime);
+            Play("MusicLoopForest");
+        }
+        
+;
+    }
+
+    public void ActivateMusic()
+    {
+        //Debug.Log(PlayerPrefs.GetInt(music));
+
+        if (PlayerPrefs.GetInt(music) == 0)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
+ 
+   
+
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+## The game
+Into the light is a 2D platform game where the character has a flashlight and only the parts of the environment hit by the light are visible to the player. The goal is to complete the map while avoiding traps. The challenge is the fact that the whole map, aside from the objects hit by the light, is completely dark.
+
+## Development
+It was developed in a group of 6 students during a 4 week period using Scrum 
+as framework for development. Non of us had previous experience with Unity.
+
+## Demo
+A demo (which is a bit buggy) of the game can be found in the repo of the group member [@djikoSal](https://github.com/djikoSal).
+
+[Into The Light (Play in the browser)](https://djikosal.github.io/)
+
+![Menu](Images/Menu.png)
+
+![Level 1](Images/Level1.png)
+
+![Level 1](Images/Level1Demo.gif)
